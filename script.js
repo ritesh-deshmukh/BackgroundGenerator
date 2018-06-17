4f5585b474c46e4177434ad9fad9672f186182e7
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-06-16 19:24:32 -0500

    Background Generator created

---
+++ script.js
@@ -0,0 +1,20 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient")
+
+// console.log(body)
+// console.log(css)
+// console.log(color1)
+// console.log(color2)
+
+function colorChange(){
+    body.style.background = "Linear-gradient(to right, "
+        + color1.value + ", "
+        + color2.value + ")";
+    css.textContent = body.style.background + ";";
+}
+
+// color1.addEventListener("input", colorChange);
+//
+// color2.addEventListener("input", colorChange);
